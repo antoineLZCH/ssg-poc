8742944de51e1e42a0bdf97ed535a76cec7f1f82
Antoine LOZACH <antoine.lozach@cacd2.fr>
2024-02-16 16:31:22 +0100

    📦 NEW: initial commit
    Signed-off-by: Antoine LOZACH <antoine.lozach@cacd2.fr>

---
+++ components/TheHeader.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const { locales, locale } = useI18n()
+const switchLocalePath = useSwitchLocalePath()
+</script>
+
+<template>
+  <nav class="flex items-center justify-between">
+    <span>TheTest</span>
+    <div class="flex items-center gap-2">
+      <NuxtLink :to="switchLocalePath('fr')">FR</NuxtLink>
+      <NuxtLink :to="switchLocalePath('en')">EN</NuxtLink>
+    </div>
+  </nav>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,26 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/i18n', '@unocss/nuxt'],
+  i18n: {
+    locales: [
+      {
+        code: 'fr',
+        iso: 'fr-FR',
+        name: 'Français',
+        file: 'fr.ts'
+      },
+      {
+        code: 'en',
+        iso: 'en-US',
+        name: 'English',
+        file: 'en.ts'
+      }
+    ],
+    langDir: 'i18n',
+    defaultLocale: 'fr',
+    vueI18n: './i18n.config.ts',
+    strategy: 'prefix_except_default',
+    detectBrowserLanguage: false,
+  }
+})
